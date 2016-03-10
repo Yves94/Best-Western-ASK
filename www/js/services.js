13e371b43797c620ea357649290059f429c95f40
Yves Legris <ylegris94@gmail.com>
2016-03-10 21:50:47 +0100

    Update path factory

--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,7 @@ angular.module('starter.services', ['ngResource'])
     return {
         loginUser: function(name, pw) {
             var def = $q.defer();
-            $http.post(CONFIG.api_url+'/user/connect', {'id':name,password:pw})
+            $http.post(CONFIG.api_url + '/user/connect', {'id':name,password:pw})
                 .success(function(data) {
                     def.resolve(data);
                 })
@@ -19,7 +19,7 @@ angular.module('starter.services', ['ngResource'])
 .factory('FavorisFactory', function($resource, $http) {
     return {
         getAll: function($scope) {
-            $http.get('http://bw.dev/api/favoris/getall').then(function(data) {
+            $http.get(CONFIG.api_url + '/favoris/getall').then(function(data) {
                 if (typeof data.data.favoris != 'undefined') {
                     $.each(data.data.favoris, function(index, value){
                         console.log(value.title);
@@ -34,7 +34,7 @@ angular.module('starter.services', ['ngResource'])
 .factory('ActivityFactory', function($resource,$http,CONFIG) {
     return {
         getAll: function($scope) {
-            $http.get(CONFIG.api_url+'/activity/getall').then(function(data) {
+            $http.get(CONFIG.api_url + '/activity/getall').then(function(data) {
                 if (typeof data.data.activities != 'undefined') {
                     $scope.activities = data.data.activities;
                 }
@@ -46,7 +46,7 @@ angular.module('starter.services', ['ngResource'])
 .factory('HotelFactory', function($resource,$http,CONFIG) {
     return {
         getAll: function($scope) {
-            $http.get(CONFIG.api_url+'/hotel/getall').then(function(data) {
+            $http.get(CONFIG.api_url + '/hotel/getall').then(function(data) {
                 if (typeof data.data.hotels != 'undefined') {
                     $scope.hotels = data.data.activities;
                 }
